a511d24f23cc5614dfb9169a76633fc7cb3e7120
NzOishie <nazzarreen05@gmail.com>
2022-04-01 11:38:42 -0600

    Added redis support

--- utils.py
+++ utils.py
@@ -2,20 +2,20 @@ import json
 import ssl
 import urllib
 
-from flask_caching import Cache
-
 import certifi
+import redis
 
-from app import app
-
-cache = Cache(app, config={'CACHE_TYPE': 'redis', 'CACHE_REDIS_URL': 'redis://localhost:6379/0'})
+r = redis.Redis(host='localhost', port=6379, db=0)
 
 
-# @cache.cached(timeout=60*60)
 def get_json_response(search_url):
-    search_response = urllib.request.urlopen(
-        search_url, context=ssl.create_default_context(cafile=certifi.where()),
-    )
-    search_result = search_response.read()
+    if r.exists(search_url):
+        search_result = r.get(search_url)
+    else:
+        search_response = urllib.request.urlopen(
+            search_url, context=ssl.create_default_context(cafile=certifi.where()),
+        )
+        search_result = search_response.read()
+        r.set(search_url,search_result)
     all_dog = json.loads(search_result)
     return all_dog
